043eee8ee6d2c8c2e2e5ea8388b4687d29865ab5
hsabapathy <hsabapathy@ucsd.edu>
2026-05-21 08:55:02 -0700

    Updated to include CBC and CTR mode

--- CPU Implementation/SKINNY-64/skinny_64_128.cpp
+++ CPU Implementation/SKINNY-64/skinny_64_128.cpp
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-//4-bit Sbox
+#define SKINNY64_ROUNDS 36
+
+// 4-bit Sbox 
 const uint8_t S[16] = {0xc, 0x6, 0x9, 0x0, 0x1, 0xa, 0x2, 0xb, 0x3, 0x8, 0x5, 0xd, 0x4, 0xe, 0x7, 0xf};
 
-//4-bit Sbox inverse
+// 4-bit Sbox inverse
 const uint8_t S_inverse[16] = {0x3, 0x4, 0x6, 0x8, 0xc, 0xa, 0x1, 0xe, 0x9, 0x2, 0x5, 0x7, 0x0, 0xb, 0xd, 0xf};
 
-
-//Permutation P[i] = x
+// Permutation P[i] = x
 const uint8_t P[16] = {0x0, 0x1, 0x2, 0x3, 0x7, 0x4, 0x5, 0x6, 0xa, 0xb, 0x8, 0x9, 0xd, 0xe, 0xf, 0xc};
 
-//Inverse Permutation P_inverse[x] = i
+// Inverse Permutation P_inverse[x] = i
 const uint8_t P_inverse[16] = {0x0, 0x1, 0x2, 0x3, 0x5, 0x6, 0x7, 0x4, 0xa, 0xb, 0x8, 0x9, 0xf, 0xc, 0xd, 0xe};
 
-// Tweakey Permutation
+// Tweakey Permutation PT
 const uint8_t Q[16] = {0x9, 0xf, 0x8, 0xd, 0xa, 0xe, 0xc, 0xb, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7};
 
-// Round Constants
-const uint8_t Round_Constant[36] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31, 0x23, 0x06, 0x0D};
+// Round Constants (6-bit LFSR)
+const uint8_t Round_Constant[36] = {
+    0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C,
+    0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30,
+    0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31, 0x23, 0x06, 0x0D
+};
+
+
 
 uint8_t hex_char_to_val(char c)
 {
@@ -32,11 +40,17 @@ uint8_t hex_char_to_val(char c)
     return 0;
 }
 
-//Function to print the state
 void print_state(uint8_t state[16])
 {
     for (int i = 0; i < 16; i++)
         printf("%01x", state[i]);
+    printf("\n");
+}
+
+void print_message(uint8_t *msg, int num_blocks)
+{
+    for (int i = 0; i < num_blocks * 16; i++)
+        printf("%01x", msg[i]);
     printf("\n");
 }
 
@@ -45,7 +59,6 @@ void convert_hex_string_to_statearray(string hex_string, uint8_t int_array[16], 
     for (size_t i = 0; i < 16; i++)
     {
         uint8_t val = hex_char_to_val(hex_string[i]);
-
         if (reversed)
             int_array[15 - i] = val;
         else
@@ -60,7 +73,7 @@ uint8_t TK2_lfsr(uint8_t x)
     return x;
 }
 
-// Function to create diffusion - Mixing 4 element column of state
+
 void mix_columns(uint8_t state[16])
 {
     uint8_t tmp;
@@ -95,278 +108,326 @@ void inverse_mix_columns(uint8_t state[16])
 
 void round_tweakey_schedule(int rounds, uint8_t TK1[][16], uint8_t TK2[][16], uint8_t round_tweakey[][8])
 {
-    //Variables to store permutated version of TK1 and TK2 
-    uint8_t TKp1[rounds - 1][16];
-    uint8_t TKp2[rounds - 1][16];
+    uint8_t TKp1[16];
+    uint8_t TKp2[16];
 
     // Masking to 4 bits
     for (uint8_t i = 0; i < 16; i++)
+    {
         TK1[0][i] = (TK1[0][i] & 0xf);
-    
-    for (uint8_t i = 0; i < 16; i++)
         TK2[0][i] = (TK2[0][i] & 0xf);
+    }
 
-    printf("TK1 Round 0 :");
-    print_state(TK1[0]);
-
-    printf("TK2 Round 0 :");
-    print_state(TK2[0]);
-
-    //Computing Round 0 tweaky RTK = TK1 ^ TK2
+    // Computing Round 0 tweakey RTK = TK1 ^ TK2
     for (uint8_t i = 0; i < 8; i++)
         round_tweakey[0][i] = TK1[0][i] ^ TK2[0][i];
 
-    printf("RTK Round 0: ");
-    print_state(round_tweakey[0]);
-    
     for (int r = 1; r < rounds; r++)
     {
         // Tweakey permutation of TK1 and TK2
         for (int i = 0; i < 16; i++)
         {
-            TKp1[r - 1][i] = TK1[r - 1][Q[i]];
-            TKp2[r - 1][i] = TK2[r - 1][Q[i]];
+            TKp1[i] = TK1[r - 1][Q[i]];
+            TKp2[i] = TK2[r - 1][Q[i]];
         }
 
-        if (r == rounds - 1)
-        {
-            printf("After Permutation TK1: ");
-            print_state(TKp1[r-1]);
-
-            printf("After Permuatation TK2: ");
-            print_state(TKp2[r -1]);
-        }
-        
-        // Apply LFSR to TK2
+        // Apply LFSR2 to first two rows of permuted TK2
         for (int i = 0; i < 16; i++)
         {
-            TK1[r][i] = TKp1[r - 1][i];
-            
-            if (i < 8)
-            {
-                TK2[r][i] = TK2_lfsr(TKp2[r - 1][i]);
-            }
-            else
-            {
-                TK2[r][i] = TKp2[r - 1][i];
-            }
-        }
-        
-        if (r == rounds - 1)
-        {
-            printf("After LFSR TK2: ");
-            print_state(TK2[r]);
+            TK1[r][i] = TKp1[i];
 
-            printf("Updated TK1 :");
-            print_state(TK1[r]);
+            if (i < 8)
+                TK2[r][i] = TK2_lfsr(TKp2[i]);
+            else
+                TK2[r][i] = TKp2[i];
         }
-        
+
+        // Compute round tweakey
         for (int i = 0; i < 8; i++)
             round_tweakey[r][i] = (TK1[r][i] ^ TK2[r][i]);
-
-        if (r == rounds - 1)
-        {
-            printf("Round Tweakey RTK :");
-            print_state(round_tweakey[r]);
-        }
     }
 }
 
-void encryption(int R, uint8_t plain_text[16], uint8_t cipher_text[16], uint8_t TK[][8])
+//Encryption main block which is called in CTR and CBC modes
+void encryption_block(int R, uint8_t in[16], uint8_t out[16], uint8_t TK[][8])
 {
     for (uint8_t i = 0; i < 16; i++)
-    {
-        cipher_text[i] = plain_text[i] & 0xf;
-    }
-    printf("Initial State: ");
-    print_state(cipher_text);
+        out[i] = in[i] & 0xf;
 
     for (uint8_t r = 0; r < R; r++)
     {
-        // S - Box
+        // S-box substitution
         for (uint8_t i = 0; i < 16; i++)
-            {
-                cipher_text[i] = S[cipher_text[i]];
-            }
-        
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After SBox: ", r);
-            print_state(cipher_text);
-        }
+            out[i] = S[out[i]];
 
-        //Adding round constants
-        cipher_text[0] ^= (Round_Constant[r] & 0xf);
-        cipher_text[4] ^= ((Round_Constant[r] >> 4) & 0x3);
-        cipher_text[8] ^= 0x2;
+        // Add round constants
+        out[0] ^= (Round_Constant[r] & 0xf);
+        out[4] ^= ((Round_Constant[r] >> 4) & 0x3);
+        out[8] ^= 0x2;
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After adding constants: ", r);
-            print_state(cipher_text);
-        }
-
-        //Adding round tweaky
+        // Add round tweakey
         for (uint8_t i = 0; i < 8; i++)
-            cipher_text[i] ^= TK[r][i];
+            out[i] ^= TK[r][i];
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After Round Tweaky: ", r);
-            print_state(cipher_text);
-        }
-        
+        // Permutation (ShiftRows)
         uint8_t temp[16];
         for (uint8_t i = 0; i < 16; i++)
-            {
-            temp[i] = cipher_text[i];
-            }
+            temp[i] = out[i];
         for (uint8_t i = 0; i < 16; i++)
-            {
-            cipher_text[i] = temp[P[i]];
-            }
-        
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After permutation: ", r);
-            print_state(cipher_text);
-        }
-        
-        mix_columns(cipher_text);
+            out[i] = temp[P[i]];
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After mix_columns: ", r);
-            print_state(cipher_text);
-        }
+        // MixColumns
+        mix_columns(out);
     }
-    printf("Final Ciphertext:");
-    print_state(cipher_text);
 }
 
-void decryption(int R, uint8_t plain_text[16], uint8_t cipher_text[16], uint8_t TK[][8])
+//Decryption main block which is called in CTR and CBC modes
+void decryption_block(int R, uint8_t in[16], uint8_t out[16], uint8_t TK[][8])
 {
-    // Initializing the state
     for (uint8_t i = 0; i < 16; i++)
-    {
-        plain_text[i] = cipher_text[i] & 0xf;
-    }
+        out[i] = in[i] & 0xf;
 
-    printf("Initial Cipher State: ");
-    print_state(plain_text);
-    
-    int index;
-    uint8_t temp[16];
-    
     for (int r = 0; r < R; r++)
     {
-        // Inverse Mix Columns
-        inverse_mix_columns(plain_text);
-        
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After inverse mix columns: ", r);
-            print_state(plain_text);
-        }
-        
-        for (uint8_t i = 0; i < 16; i++)
-            temp[i] = plain_text[i];
-        
-        //Performing inverse permuatation
-        for (uint8_t i = 0; i < 16; i++)
-            plain_text[i] = temp[P_inverse[i]];
+        inverse_mix_columns(out);
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After inverse permuatation: ", r);
-            print_state(plain_text);
-        }
-        
-        index = R - r - 1;
-        //Removing Tweakey
+        uint8_t temp[16];
+        for (uint8_t i = 0; i < 16; i++)
+            temp[i] = out[i];
+        for (uint8_t i = 0; i < 16; i++)
+            out[i] = temp[P_inverse[i]];
+
+        int index = R - r - 1;
+        // Remove tweakey
         for (uint8_t i = 0; i < 8; i++)
-            plain_text[i] ^= TK[index][i];
+            out[i] ^= TK[index][i];
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After Tweaky XOR: ", r);
-            print_state(plain_text);
-        }
-        
-        // Removing the constants
-        plain_text[0] ^= (Round_Constant[index] & 0xf);
-        plain_text[4] ^= ((Round_Constant[index] >> 4) & 0x3);
-        plain_text[8] ^= 0x2;
+        // Remove round constants
+        out[0] ^= (Round_Constant[index] & 0xf);
+        out[4] ^= ((Round_Constant[index] >> 4) & 0x3);
+        out[8] ^= 0x2;
 
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After removal of constants: ", r);
-            print_state(plain_text);
-        }
-        
-        // Inverse S-Box
+        // Inverse S-box
         for (uint8_t i = 0; i < 16; i++)
-            plain_text[i] = S_inverse[plain_text[i]];
-
-        if (r == 0 || r == R - 1)
-        {
-            printf("Round %d - After inverse S-Box: ", r);
-            print_state(plain_text);
-        }
-    
+            out[i] = S_inverse[out[i]];
     }
-    printf("Final recovered Plain_text :");
-    print_state(plain_text);
 }
+
+// Increment counter ; nibble wise
+void increment_counter(uint8_t counter[16])
+{
+    for (int i = 15; i >= 0; i--)
+    {
+        counter[i] = (counter[i] + 1) & 0xf;
+        if (counter[i] != 0) break;
+    }
+}
+
+//CTR mode encryption
+void ctr_encrypt(int R, uint8_t *plaintext, uint8_t *ciphertext,
+                 int num_blocks, uint8_t nonce[16], uint8_t TK[][8])
+{
+    uint8_t counter[16];
+    uint8_t keystream[16];
+
+    // Initialize counter with nonce
+    for (int i = 0; i < 16; i++)
+        counter[i] = nonce[i];
+
+    for (int b = 0; b < num_blocks; b++)
+    {
+        // Encrypt counter block to produce keystream
+        encryption_block(R, counter, keystream, TK);
+
+        // XOR plaintext nibbles with keystream
+        for (int i = 0; i < 16; i++)
+            ciphertext[b * 16 + i] = (plaintext[b * 16 + i] ^ keystream[i]) & 0xf;
+
+        // Increment counter for next block
+        increment_counter(counter);
+    }
+}
+
+// CTR mode decryption
+void ctr_decrypt(int R, uint8_t *ciphertext, uint8_t *plaintext,
+                 int num_blocks, uint8_t nonce[16], uint8_t TK[][8])
+{
+    ctr_encrypt(R, ciphertext, plaintext, num_blocks, nonce, TK);
+}
+
+//CBC mode encryption
+void cbc_encrypt(int R, uint8_t *plaintext, uint8_t *ciphertext,
+                 int num_blocks, uint8_t iv[16], uint8_t TK[][8])
+{
+    uint8_t prev[16];
+    uint8_t block_in[16];
+
+    // Initialize with IV
+    for (int i = 0; i < 16; i++)
+        prev[i] = iv[i];
+
+    for (int b = 0; b < num_blocks; b++)
+    {
+        // XOR plaintext with previous ciphertext
+        for (int i = 0; i < 16; i++)
+            block_in[i] = (plaintext[b * 16 + i] ^ prev[i]) & 0xf;
+
+        // Encrypt the XOR-ed block
+        encryption_block(R, block_in, &ciphertext[b * 16], TK);
+
+        // Update chaining value
+        for (int i = 0; i < 16; i++)
+            prev[i] = ciphertext[b * 16 + i];
+    }
+}
+
+// CBC mode decryption
+void cbc_decrypt(int R, uint8_t *ciphertext, uint8_t *plaintext,
+                 int num_blocks, uint8_t iv[16], uint8_t TK[][8])
+{
+    uint8_t prev[16];
+    uint8_t block_out[16];
+    uint8_t saved_ct[16];
+
+    // Initialize with IV
+    for (int i = 0; i < 16; i++)
+        prev[i] = iv[i];
+
+    for (int b = 0; b < num_blocks; b++)
+    {
+        // Saving the ciphertext block before decryption
+        for (int i = 0; i < 16; i++)
+            saved_ct[i] = ciphertext[b * 16 + i];
+
+        // Decrypting the ciphertext block
+        decryption_block(R, &ciphertext[b * 16], block_out, TK);
+
+        // XOR with previous ciphertext (or IV) to recover plaintext
+        for (int i = 0; i < 16; i++)
+            plaintext[b * 16 + i] = (block_out[i] ^ prev[i]) & 0xf;
+
+        // Update chaining value
+        for (int i = 0; i < 16; i++)
+            prev[i] = saved_ct[i];
+    }
+}
+
+//Validating the results
+int assert_equal(const char *label, uint8_t *got, uint8_t *expected, int len)
+{
+    if (memcmp(got, expected, len) == 0)
+    {
+        printf("The recovered plaintext matches the original plaintext %s\n", label);
+        return 1;
+    }
+    printf("The recovered plaintext doesnt match the original plaintext %s\n       got:      ", label);
+    for (int i = 0; i < len; i++) printf("%01x", got[i]);
+    printf("\n       expected: ");
+    for (int i = 0; i < len; i++) printf("%01x", expected[i]);
+    printf("\n");
+    return 0;
+}
+
 
 int main()
 {
-    uint8_t plain_text[16];
-    uint8_t cipher_text[16];
+   
+    printf("--------SKINNY-64-128 Implementation--------\n");
+    printf("--------CTR and CBC Mode Tests--------\n");
+   
+    int R = SKINNY64_ROUNDS;
 
-    //Number of rounds
-    int R = 36;
-    
     uint8_t TK_1[R][16];
     uint8_t TK_2[R][16];
     uint8_t RTK[R][8];
     uint8_t tweakey_1[16];
     uint8_t tweakey_2[16];
+
     
-    // Test vectors
+    //Test vectors
     string TK1_string = "9eb93640d088da63";
     string TK2_string = "76a39d1c8bea71e1";
-    string plain_string = "cf16cfe8fd0f98aa";
-    string cipher_string = "6ceda1f43de92b9e";
-    
-    bool reversed = false;
-    
-    convert_hex_string_to_statearray(TK1_string, tweakey_1, reversed);
-    convert_hex_string_to_statearray(TK2_string, tweakey_2, reversed);
-    convert_hex_string_to_statearray(plain_string, plain_text, reversed);
-    
+
+    convert_hex_string_to_statearray(TK1_string, tweakey_1, false);
+    convert_hex_string_to_statearray(TK2_string, tweakey_2, false);
+
     for (uint8_t i = 0; i < 16; i++)
     {
         TK_1[0][i] = tweakey_1[i];
         TK_2[0][i] = tweakey_2[i];
     }
-    printf("\n-------Tweaky Schedule---------------\n");
-   round_tweakey_schedule(R, TK_1, TK_2, RTK);
-    
-    printf("\n------Encryption---------------------\n");
-    encryption(R, plain_text, cipher_text, RTK);
-   
-    printf("\n------Decryption---------------------\n");
-    decryption(R, plain_text, cipher_text, RTK);
 
-    printf("\n------Final Results------------------\n");
-    printf("Original Plaintext   : %s\n", plain_string.c_str());
+    printf("Generating tweakey schedule\n");
+    round_tweakey_schedule(R, TK_1, TK_2, RTK);
+    printf("Tweakey schedule generated\n\n");
 
-    printf("Final Ciphertext     : ");
-    print_state(cipher_text);
+    //CTR mode
+    printf("--------CTR Mode--------\n\n");
+
+    int num_blocks = 1;
+    uint8_t ctr_plain[16];
+    uint8_t ctr_cipher[16];
+    uint8_t ctr_recovered[16];
+
+    // Block 0 is the test plaintext and block 1 is ascending nibbles
+    string plain_string = "cf16cfe8fd0f98aa";
+    uint8_t plain_text_0[16];
+    convert_hex_string_to_statearray(plain_string, plain_text_0, false);
+
+    for (int i = 0; i < 16; i++) ctr_plain[i] = plain_text_0[i];
+
+    uint8_t nonce[16] = {0};
+
+    printf("Original Plaintext   : ");
+    print_message(ctr_plain, num_blocks);
+
+    ctr_encrypt(R, ctr_plain, ctr_cipher, num_blocks, nonce, RTK);
+
+    printf("CTR Ciphertext       : ");
+    print_message(ctr_cipher, num_blocks);
+
+    ctr_decrypt(R, ctr_cipher, ctr_recovered, num_blocks, nonce, RTK);
 
     printf("Recovered Plaintext  : ");
-    print_state(plain_text);   
+    print_message(ctr_recovered, num_blocks);
+
+    printf("\n");
+    assert_equal("\nCTR mode is validated",
+                 ctr_recovered, ctr_plain, num_blocks * 16);
+
+    // CBC mode
+    printf("\n--------CBC Mode--------\n\n");
+
+    uint8_t cbc_plain[16];
+    uint8_t cbc_cipher[16];
+    uint8_t cbc_recovered[16];
+
+   
+    for (int i = 0; i < 16; i++) cbc_plain[i] = plain_text_0[i];
+
+    // IV is set to all zeros
+    uint8_t iv[16] = {0};
+
+    printf("Original Plaintext   : ");
+    print_message(cbc_plain, num_blocks);
+
+    cbc_encrypt(R, cbc_plain, cbc_cipher, num_blocks, iv, RTK);
+
+    printf("CBC Ciphertext       : ");
+    print_message(cbc_cipher, num_blocks);
+
+    cbc_decrypt(R, cbc_cipher, cbc_recovered, num_blocks, iv, RTK);
+
+    printf("Recovered Plaintext  : ");
+    print_message(cbc_recovered, num_blocks);
+
+    printf("\n");
+    assert_equal("\nCBC mode is validated ",
+                 cbc_recovered, cbc_plain, num_blocks * 16);
+
+   
+    printf("--------All tests have completed--------\n");
+
 
     return 0;
 }
